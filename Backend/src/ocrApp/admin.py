8d021903665512604dd476001fab2f94fd59d68d
gethinzhu <gethinzhu@gmail.com>
2026-04-26 20:33:18 +0800

    Register contact messages in admin

--- Backend/src/ocrApp/admin.py
+++ Backend/src/ocrApp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import OCRImage
+from .models import ContactMessage, OCRImage
 
 
 @admin.register(OCRImage)
@@ -8,4 +8,12 @@ class OCRImageAdmin(admin.ModelAdmin):
     list_filter = ("status",)
     search_fields = ("original_filename",)
     readonly_fields = ("id", "created_at", "updated_at")
+    ordering = ("-created_at",)
+
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "subject", "created_at")
+    search_fields = ("name", "email", "subject", "message")
+    readonly_fields = ("id", "created_at")
     ordering = ("-created_at",)
